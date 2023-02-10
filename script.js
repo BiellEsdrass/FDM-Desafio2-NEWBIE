7a9f259957ba059c21e3727fa625c06f90ba5a06
BiellEsdrass <gabriell.com.br13@gmail.com>
2023-02-09 22:12:46 -0300

    Desafio 2 - FrontEnd Mentor

---
+++ script.js
@@ -0,0 +1,16 @@
+const iconView = document.getElementById('iconView');
+const blurBg = document.getElementById('imgPrincipal');
+
+
+function blurOn(){
+    blurBg.src='images/image-equilibriumBLUR.jpg'
+}
+function blurOff(){
+    blurBg.src='images/image-equilibrium.jpg'
+}
+
+
+
+
+blurBg.addEventListener('mouseover', blurOn);
+blurBg.addEventListener('mouseleave', blurOff);
